a5ce43d25c38b9adafd0c68114ae7cf60df0b4b0
sivaramsj <sivaramsj003@gmail.com>
2024-01-02 23:33:32 +0530

    Quiz app is create with basic layout and functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import questions from "./components/questions";
+import React from "react";
 
 function App() {
+  const [QuestionIndex,setQuestionIndex]=useState(0)
+  const [Score,setScore]=useState(0)
+  const [result,setResult]=useState(false)
+
+  const replay=()=>{
+    setQuestionIndex(0);
+    setScore(0);
+    setResult(false);
+  }
+
+  const present_question=questions[QuestionIndex]
+  const get_choice=(index)=>{
+    if(present_question.answer===index){
+      setScore(Score+1)
+    }
+    const next_question=QuestionIndex+1 
+    if(next_question < questions.length)
+    {
+        setQuestionIndex(QuestionIndex+1)
+    }
+    else{
+      setResult(true)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='quiz_box'>
+      {result?(<>
+      <h1>{Score}</h1>
+      <button onClick={replay}>Play Again</button>
+      </>)     
+      :     
+      (<div className="quiz_question">
+        {present_question.question}
+        <div className="quiz_box_options"></div>
+        <ul className='quiz_ul'>
+          {present_question.options.map((option,i) =>{
+            return <li className='quiz_li' onClick={()=> get_choice(i)}>{option}</li>
+          })}
+        </ul>
+      </div>)}
+      
     </div>
   );
 }
---
+++ src/components/questions.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        question: "Capital of France",
+        options: ["Paris", "Rome", "Berlin", "Madrid"],
+        answer: 0,
+    },
+    {
+        question: "Largest planet in the solar system",
+        options: ["Venus", "Jupiter", "Mars", "Saturn"],
+        answer: 1,
+    },
+    {
+        question: "Chemical symbol for gold",
+        options: ["Au", "Ag", "Fe", "Cu"],
+        answer: 0,
+    },
+    {
+        question: "Number of continents on Earth",
+        options: ["Five", "Six", "Seven", "Eight"],
+        answer: 2,
+    },
+    {
+        question: "Fastest land animal",
+        options: ["Cheetah", "Lion", "Gazelle", "Leopard"],
+        answer: 0,
+    },
+];
